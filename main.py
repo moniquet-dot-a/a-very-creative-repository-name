6cee6f51474df8a8dfe739bdb65f4471c0dfec1a
moniquet-dot-a <145666181+moniquet-dot-a@users.noreply.github.com>
2026-01-21 11:20:36 +0200

    Анкета

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Анкета студента")
+print("----------------")
+
+name = "Блищук Георгій"
+age = 16
+status = "Студент"
+group = "КІ-25"
+city = "Київ"
+future_job = "Працювати в IT-компанії"
+
+print("Ім'я:", name)
+print("Вік:", age)
+print("Статус:", status)
+print("Група:", group)
+print("Місто проживання:", city)
+print("Плани на майбутнє:", future_job)
